b01d9122a991598958f1581b2004961de1f220d2
李铭昕 <715557344@qq.com>
2020-07-11 09:39:46 +0800

    Update SessionController.php

--- app/Controller/SessionController.php
+++ app/Controller/SessionController.php
@@ -12,7 +12,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Hyperf\Guzzle\ClientFactory;
+use Hyperf\HttpMessage\Cookie\Cookie;
+use Hyperf\HttpMessage\Stream\SwooleStream;
 use Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\Utils\Codec\Json;
 
 /**
  * @AutoController(prefix="session")
@@ -21,13 +24,32 @@ class SessionController extends Controller
 {
     public function index()
     {
+        $response = $this->response->response();
+        return $response->withAddedHeader('ID', uniqid())
+            ->withAddedHeader('ID', uniqid())
+            ->withCookie(new Cookie('ID', uniqid()))
+            ->withBody(new SwooleStream(Json::encode([
+                'code' => 0,
+                'data' => 'Hello World.',
+            ])));
         return $this->response->success('Hello World.');
     }
 
     public function index2()
     {
         $client = di()->get(ClientFactory::class)->create();
+        $response = $client->get('http://127.0.0.1:9501/session/index');
+        $headers = $response->getHeaders();
+        foreach ($headers as $key => $header) {
+            $response = $response->withoutHeader($key);
+        }
+        return $response;
+    }
 
-        return $client->get('http://127.0.0.1:9501/session/index');
+    public function baidu()
+    {
+        $client = di()->get(ClientFactory::class)->create();
+
+        return $client->get('http://www.baidu.com/');
     }
 }
