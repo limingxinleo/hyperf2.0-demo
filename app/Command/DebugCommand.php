17bece8db6793a933080ab169d8f93f017afeeaf
李铭昕 <715557344@qq.com>
2020-08-04 12:14:15 +0800

    Create DebugCommand.php

---
+++ app/Command/DebugCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Command;
+
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as HyperfCommand;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Command
+ */
+class DebugCommand extends HyperfCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    protected $signature = 'test:test {id : user_id} {--name= : user_name}';
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        parent::__construct('test:test');
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this->setDescription('Hyperf Demo Command');
+    }
+
+    public function handle()
+    {
+        var_dump($this->input->getArguments());
+        var_dump($this->input->getOptions());
+    }
+}
