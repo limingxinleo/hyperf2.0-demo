513ec1b69c14d1a6afddfef50be792a9662210f5
李铭昕 <715557344@qq.com>
2020-10-01 12:54:18 +0800

    Update redis.php

--- config/autoload/redis.php
+++ config/autoload/redis.php
@@ -23,5 +23,38 @@ return [
             'heartbeat' => -1,
             'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
         ],
+        'cluster' => [
+            'enable' => false,
+            'name' => null,
+            'seeds' => [],
+        ],
+        'sentinel' => [
+            'enable' => false,
+            'master_name' => env('REDIS_MASTER_NAME', 'mymaster'),
+            'nodes' => explode(';', env('REDIS_SENTINEL_NODE', '')),
+            'persistent' => '',
+            'read_timeout' => 0,
+        ],
+    ],
+    'sentinel' => [
+        'host' => env('REDIS_HOST', 'localhost'),
+        'auth' => env('REDIS_AUTH', null),
+        'port' => (int) env('REDIS_PORT', 6379),
+        'db' => (int) env('REDIS_DB', 0),
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 10,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+            'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+        ],
+        'sentinel' => [
+            'enable' => (bool) env('REDIS_SENTINEL_ENABLE', false),
+            'master_name' => env('REDIS_MASTER_NAME', 'mymaster'),
+            'nodes' => explode(';', env('REDIS_SENTINEL_NODE', '')),
+            'persistent' => '',
+            'read_timeout' => 0,
+        ],
     ],
 ];
